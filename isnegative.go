ebfc0425abbd3513c05a2d7d31a8ed8ad87b8bc5
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-16 21:03:48 +0600

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -5,11 +5,10 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	a := 'T'
 	b := 'F'
-	if nb < 0 { 
+	if nb < 0 {
 		z01.PrintRune(a)
 	} else {
 		z01.PrintRune(b)
 	}
 	z01.PrintRune(10)
 }
-
