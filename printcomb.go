1709cd55c55dc68bf7fc708451463a46f58a7e0a
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-16 21:21:39 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for a := 0; a <= 999; a++ {
-		b = a / 100
-		c = a / 10
-		d = a % 10
+		b := a / 100
+		c := a / 10
+		d := a % 10
 		if b < c < d {
 			z01.PrintRune(a)
 		}
