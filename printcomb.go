cfd99301894e26f9fb000d5720537314b29c367f
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-16 21:49:19 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -3,7 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := 0; a <= 999; a++ {
+	for a := 1; a <= 99; a++ {
+		c := a / 10
+		d := a % 10
+		if (c < d) {
+			z01.PrintRune(rune('0'))
+			z01.PrintRune(rune(a + 60))
+		}
+	}
+
+	for a := 100; a <= 999; a++ {
 		b := a / 100
 		c := a / 10
 		d := a % 10
