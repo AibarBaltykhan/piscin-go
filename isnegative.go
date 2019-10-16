858b3cea331ba101fbb7ae2d644537c73bb3357e
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-16 21:01:52 +0600

    isnegative

---
+++ isnegative.go
@@ -0,0 +1,15 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) {
+	a := 'T'
+	b := 'F'
+	if nb < 0 { 
+		z01.PrintRune(a)
+	} else {
+		z01.PrintRune(b)
+	}
+	z01.PrintRune(10)
+}
+
