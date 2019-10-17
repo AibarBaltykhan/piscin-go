a9a94b701ea4fb85be745534a685aa004e10c89d
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-17 20:21:50 +0600

    point

--- pointone.go
+++ pointone.go
@@ -1,18 +1,5 @@
 package piscine
 
-package main
-
-import (
-    "fmt"
-    piscine ".."
-)
-
 func PointOne(n *int) {
 	*n = 1
-}
-
-func main() {
-    n := 0
-    piscine.PointOne(&n)
-    fmt.Println(n)
 }
